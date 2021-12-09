218815bb47df5749126f4c264675cc026b26333d
Yuri Godke <yurigodke@gmail.com>
2021-12-09 08:15:17 -0300

    Adds asset handler

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const fs = require('fs');
+const svgToMiniDataURI = require('mini-svg-data-uri');
 
 const htmlPageNames = fs.readdirSync('./src');
 
@@ -9,8 +10,8 @@ const entrys = {}
 const multipleHtml = htmlPageNames.map(name => {
   entrys[name] = `./src/${name}/index.js`;
   return new HtmlWebpackPlugin({
-    template: `src/${name}/index.htm`, // relative path to the HTML files
-    filename: `${name}.htm`, // output HTML files,
+    template: `src/${name}/index.htm`,
+    filename: `${name}.htm`,
     chunks: [name]
   })
 });
@@ -32,6 +33,56 @@ module.exports = {
         }
       },
       {
+        test: /\.htm(l)?$/,
+        loader: 'html-loader',
+        options: {
+          preprocessor: (content, loaderContext) => {
+            let result;
+
+            try {
+              const aliasObj = loaderContext._compiler.options.resolve.alias;
+              result = content;
+              for (var aliasKey in aliasObj) {
+                var re = new RegExp(aliasKey, 'g');
+                result = result.replace(re, aliasObj[aliasKey]);
+              }
+            } catch (error) {
+              loaderContext.emitError(error);
+
+              return content;
+            }
+
+            return result;
+          }
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'javascript/auto',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[hash].[ext]',
+              outputPath: 'assets/images',
+              esModule: false,
+            }
+          },
+        ],
+      },
+      {
+        test: /\.svg$/i,
+        type: 'javascript/auto',
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              generator: (content) => svgToMiniDataURI(content.toString())
+            },
+          },
+        ],
+      },
+      {
         test: /\.s[ac]ss$/i,
         use: [
           process.env.NODE_ENV !== 'production'
@@ -39,17 +90,20 @@ module.exports = {
             : MiniCssExtractPlugin.loader,
           'css-loader',
           'sass-loader'
-        ]
+        ],
       }
     ]
   },
   plugins: [
     ...multipleHtml,
     new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
       filename: 'assets/css/[name].css',
       chunkFilename: '[id].css'
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@images': path.resolve(__dirname, 'images'),
+    }
+  }
 };
